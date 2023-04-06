6d626f989669a6ef613298bc40b731fe57d6977f
Rohan Rajput <itusharsingh27@gmail.com>
2023-04-07 02:40:11 +0530

    commands for help in python

---
+++ help1.py
@@ -0,0 +1,3 @@
+print (dir(name)) #to get all available options
+print (help(str)) #everything about strings
+print (help(str.lower)) #to get help about a specific topic (here lower case) in strings(str)
